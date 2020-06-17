319549fd8be9106d6e07afb07a55e921474694e5
DanHazard <daniel.love.jr@gmail.com>
2020-06-16 22:17:59 -0500

    Done.

---
+++ spec/post_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+#These are lowkey bullshit btw and rarely work, it's not heling me learn
+
+# describe 'new post' do
+#   it 'ensures that the form route works with the /new action' do
+#     visit new_post_path
+#     expect(page.status_code).to eq(200)
+#   end
+#
+#   it 'renders HTML in the /new template' do
+#     visit new_post_path
+#     expect(page).to have_content('Post Form')
+#   end
+# end
